be8c318dee087465d095866bb48bee586300ee11
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-16 13:43:44 -0700

    js Asisignment

---
+++ mathExpression.js
@@ -0,0 +1,158 @@
+//Q1 
+// var a=5;
+// var b=2;
+
+// var result =a+b;
+// document.write("Your Answer is : ",result);
+
+
+
+//Q2 part b******************************************
+    // var a=5;
+    // var b=2;
+
+    // var result=a-b;
+    // document.write("Your Answer is : ",result);
+
+
+
+//Q2 part c****************************************************
+
+    // var a=5;
+    // var b=2;
+
+    // var result=a*b;
+    // document.write("Multiplication : ",result);
+
+//Q2 part d
+
+// var a=4;
+// var b=2;
+
+// var result=a/b;
+// document.write("Multiplication : ",result);
+
+
+//Q 3 ***************************************************
+
+// var a;
+// document.write(" Value after variable declaration is :",a);
+// a=5;
+// document.write("<br /> initial value is :" ,a);
+//  a++;
+//  document.write("<br /> Value after increment is : ",a);
+//  var a=a+7;
+//  document.write("<br /> value after adding 7 : ",a);
+//  a--;
+//  document.write("<br /> value after decriment is : ",a)
+//  a=a%3;
+//  document.write("<br /> reminder when divided by 3 is : ",a)
+
+
+
+
+//Q 4 *****************************************************
+
+// var ticket=600;
+// document.write("Cost of one movie ticket is :",ticket);
+
+// var ticket5 =ticket*5;
+
+// document.write("<br /> Cost of Five tickets is : ",ticket5);
+
+
+
+
+//Q5 ****************************************************
+// var multi = parseInt(prompt());
+// document.write("Table of ",multi)
+// for (var a=1; a<=10;a++){
+//    var result = a * multi;
+//    document.write("<br />",multi," *",a," = ",result);
+// }
+
+
+
+// Q6
+    // var F = parseInt(prompt('Enter F'));
+    // var C=(F-32)*5/9;
+    // document.write(F,"<sup>0</sup>F is ",C,"<sup>0</sup>C")
+
+
+    // var C = parseInt(prompt('Enter C'));
+    // var F=(C*9/5)+32
+    // document.write("</br>", C,"<sup>0</sup>C  is " ,F,"<sup>0</sup>F")
+
+
+//Q7********************************
+
+// var a =" Price of item 1 ";
+// var b= "Price of item 2 ";
+// var c= "Ordered quantity of item 1 ";
+// var d= "Ordered Quantity of item 2 ";
+// var e= "Shipping charges ";
+
+// var pItem1= 650;
+// var QuanItem1= 3;
+// var pItem2= 100;
+// var QuanItem2= 7;
+// var shipCharg=100;
+
+// var total=(pItem1*QuanItem1)+(pItem2*QuanItem2)+shipCharg;
+// document.write(a+pItem1,"</br>",c+QuanItem1,"</br>",b+pItem2,"</br>",d+QuanItem2,"</br>",e+total);
+
+//Q8**********************************
+
+// var subject1=90;
+// var subject2=80;
+// var total=200;
+// var marksgain=subject1+subject2;
+
+// var percent=(marksgain/total)*100;
+// document.write("Total marks ",total,"<br>")
+// document.write("Marks gain ",marksgain,"<br>")
+// document.write("Percentage ",percent,"<br>")
+
+
+//Q9***************************
+
+
+    // var us$=104.80;
+    // var saudiR=28;
+
+    // var convert$=us$*10;
+    // var convertRiyal=saudiR*25;
+    // var total=convert$+convertRiyal;
+    // document.write("Total Currency in PKR "+total)
+
+
+    //Q10********************************
+
+//    var number=5;
+//     var exp=((number+5)*10)/2; 
+
+//Q11*********************
+
+
+
+
+//12
+
+// var radius=20;
+// var circum=2*3.142*radius;
+// var area=3.142*(radius*radius);
+// document.write("Radius of circle : "+radius+"<br>");
+// document.write("circumferrnce  of circle : "+circum+"<br>")
+// document.write("Radius of circle : "+area+"<br>")
+
+
+
+
+//Q13
+var fav="charryballs";
+var currentAge=22;
+var maxAge=80;
+var snakePerDay=2;
+
+var totalRIP=(maxAge-currentAge)*snakePerDay*365;
+document.write("snaks for life time "+totalRIP)
